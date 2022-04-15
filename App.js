878b169a09df8e5219a5b3563312f0335ef2b7cd
Rodrigo Luiz <99453762+NodiMcXarpi@users.noreply.github.com>
2022-04-14 22:28:11 -0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,30 @@
+import React, {useState} from 'react'
+import './App.css';
+
+export default function App() {
+  const [num, setNum]=useState(10)
+  const [oxe, setOxe]=useState('vish')
+  const [sd9, setSd9]=useState('Ela quer mi da di quatu')
+  const [baseado, setPica]=useState('ela acende o baseaduuu')
+  let n1 = 10
+  const oxente=()=>{
+    console.log('oxi mainha')
+  }
+ 
+  return (
+   <>
+    <h1>{sd9}</h1>
+    <button onClick={()=>setSd9('eu quero ela por cimaaaaa')}>Por cimAAAaaaAAA</button>
+   <p>Valor do state num: {num}</p>
+   <h2>{baseado}</h2>
+   <button onClick={()=>setPica('eu empurru a minha pica')}>baseado</button>
+   <p><button onClick={()=>setNum('oxente') + oxente()}>oxente button</button></p>
+   <p>Paulistano q se acha brabo disse: {oxe}</p>
+   <button onClick={()=>setOxe('oxi mainha')}>baianoTalks</button>
+
+   
+  
+   </>
+  );
+}
+
